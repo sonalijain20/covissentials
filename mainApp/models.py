aa14ca0da3556c6aaffaec90e0d7cfa178b3578a
sonaljain20 <sonalijn20@gmail.com>
2021-05-10 18:13:05 +0530

    table

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    uname = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, default=None, null=True, blank=True)
+    address = models.CharField(max_length=20, default=None, null=True, blank=True)
+    landmark = models.CharField(max_length=20, default=None, null=True, blank=True)
+    city = models.CharField(max_length=20, default=None, null=True, blank=True)
+    state = models.CharField(max_length=20, default=None, null=True, blank=True)
+    pin = models.CharField(max_length=20, default=None, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id) + " " + self.name
+
+
+class Resources(models.Model):
+    resource = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.id) + " " + self.name
